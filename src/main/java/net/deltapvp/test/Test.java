42fe32ef3fec019227af9def66ea6e61edc4378e
powercas_gamer <cas@deltapvp.net>
2023-05-27 17:08:20 +0200

    reproduce

---
+++ src/main/java/net/deltapvp/test/Test.java
@@ -0,0 +1,11 @@
+package net.deltapvp.test;
+
+public class Test {
+
+    public static void main(final String[] args) {
+        System.out.println("Hello, world!");
+
+        boolean isEmpty = "".isEmpty();
+        boolean isBlank = "".isBlank();
+    }
+}
